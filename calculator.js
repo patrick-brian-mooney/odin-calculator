a05994d566109c75fc670dfbdb4d2d598821ed06
Patrick Mooney <github.pmooney78@neverbox.com>
2026-02-04 14:11:07 -0600

    Add solution to first part of assignment

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function testFuncs() {
+    console.log(add(8, 2));
+    console.log(subtract(8, 2));
+    console.log(multiply(8, 2));
+    console.log(divide(8, 2));
+}
+
